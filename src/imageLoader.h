9187311ea39fe6c01c060905a4821b07f452d790
Dmirty <dimonchik0036@gmail.com>
2016-12-28 02:42:09 +0700

    Update and rename picture.h to imageLoader.h

---
+++ src/imageLoader.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+SDL_Surface* loadImage(char *filename);
--- src/picture.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <SDL.h>
-#include <SDL_image.h>
-
-SDL_Texture* loadTexture(char *file, SDL_Renderer *renderer);
