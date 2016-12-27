d4e74ae1f12782cc24b0cd1ed81eb61e98eeba25
Dmirty <dimonchik0036@gmail.com>
2016-12-28 02:41:31 +0700

    Reloading

---
+++ src/imageLoader.c
@@ -0,0 +1,24 @@
+#include "imageLoader.h"
+
+SDL_Surface* loadImage(char *filename)
+{
+	SDL_Surface *inputImage = NULL;
+	SDL_Surface *resultImage = NULL;
+	
+	inputImage = IMG_Load(filename);
+
+	if (inputImage != NULL)
+	{
+		resultImage = SDL_DisplayFormatAlpha(inputImage);
+
+		SDL_FreeSurface(inputImage);
+	}
+	else
+	{
+		printf("Error loading image %s\nForce quit.\n", filename);
+
+		exit(1);
+	}
+
+	return resultImage;
+}
--- src/picture.c
+++
@@ -1,8 +0,0 @@
-#include "picture.h"
-
-SDL_Texture* loadTexture(char *file, SDL_Renderer *renderer)
-{
-	SDL_Texture *texture = IMG_LoadTexture(renderer, file);
-
-	return texture;
-}
