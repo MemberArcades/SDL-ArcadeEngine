36436598cbb5606f46fce8b44d66f8a91cd60821
Dmirty <dimonchik0036@gmail.com>
2016-12-28 17:41:05 +0700

    v1.1

--- src/imageloader.c
+++ src/imageloader.c
@@ -1,6 +1,6 @@
 #include "imageloader.h"
 
-SDL_Surface *load_file_image(char *filename)
+SDL_Surface *load_image(char *filename)
 {
 	SDL_Surface *inputImage = NULL;
 	SDL_Surface *resultImage = NULL;
@@ -21,32 +21,4 @@ SDL_Surface *load_file_image(char *filename)
 	}
 
 	return resultImage;
-}
-
-void load_image(char *filename, int x, int y, SDL_Rect *iM, SDL_Surface* screen)
-{
-	SDL_Surface *image = NULL;
-
-	if ((image = load_file_image(filename)) == NULL)
-	{
-		printf("Error loading image %s\nForce quit.\n", filename);
-
-		exit(1);
-	}
-
-	SDL_Rect offset;
-
-	offset.x = x;
-	offset.y = y;
-
-	if (iM == NULL)
-	{
-		SDL_BlitSurface(image, NULL, screen, &offset);
-	}
-	else
-	{
-		SDL_BlitSurface(image, iM, screen, &offset);
-	}
-
-	SDL_FreeSurface(image);
 }
