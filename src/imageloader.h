5dbedff805337a79ab39a1026165adc1922f2773
Dmirty <dimonchik0036@gmail.com>
2016-12-28 17:39:59 +0700

    v1.1

--- src/imageloader.h
+++ src/imageloader.h
@@ -3,11 +3,4 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
-SDL_Surface *load_file_image(char *filename);
-
-/* filename - это путь до изображения
-   x, y - это координаты точки, куда вставляется изображение
-   iM может равняться NULL, тогда выводится изображение целиком, 
-   при наличии iM выводится та часть изображения, которая
-   соответствует заданным параметрам*/
-void load_image(char *filename, int x, int y, SDL_Rect *iM, SDL_Surface* screen);
+SDL_Surface *load_image(char *filename);
