17762f7b4213903f7ffe5a4eebf3e68eb48cdcea
Dmirty <dimonchik0036@gmail.com>
2016-12-28 18:06:59 +0700

    v1.0

--- src/renderer.h
+++ src/renderer.h
@@ -2,7 +2,15 @@
 
 #include <stdbool.h>
 #include "SDL.h"
+#include "imageloader.h"
 
 void draw_point(SDL_Surface *surface, int x, int y, Uint32 color);
 
 void draw_line(SDL_Surface *surface, int x0, int y0, int x1, int y1, Uint32 color);
+
+/*filename - это путь до изображения
+x, y - это координаты точки, куда вставляется изображение
+iM может равняться NULL, тогда выводится изображение целиком,
+при наличии iM выводится та часть изображения, которая
+соответствует заданным параметрам*/
+void draw_image(char *filename, int x, int y, SDL_Rect *iM, SDL_Surface* screen);
