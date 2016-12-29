13cd2eb0725780e79a24b69006b95691189424db
BanyRule <banyrule@gmail.com>
2016-12-29 14:33:38 +0000

    style-fix

--- src/input.h
+++ src/input.h
@@ -5,12 +5,11 @@
 
 #define MAX_KEYS 323
 
+/* Обработчик нажатия кнопки */
 void handle_keydown(int keycode);
 
+/* Обработчик отпуская кнопки */
 void handle_keyup(int keycode);
 
-/**
- * Проверяет, нажата ли заданная кнопка. 
- * \keycode: SDLK константа.
- */
+/* Проверяет, нажата ли заданная кнопка. */
 bool key_held(int keycode);
