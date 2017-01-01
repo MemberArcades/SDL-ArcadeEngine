dc24e38ebefe024f642f058a2e05942d8372d424
BanyRule <Banyrule@gmail.com>
2017-01-01 18:30:07 +0700

    Добавил структура Collider, функцию "заворачивания" краев экрана

--- src/physicsbody.h
+++ src/physicsbody.h
@@ -1,10 +1,26 @@
 #pragma once
 
+#include <stdbool.h>
+
 #include "vector.h"
+#include "main.h"
+
+typedef struct
+{
+	Vector points[16];
+	int  points_count;
+} Collider;
 
 typedef struct
 {
 	Vector position;
 	Vector direction;
+	Collider collider;
 	double velocity;
 } PhysicsBody;
+
+void move_physics_body(PhysicsBody *body);
+
+void rotate_physics_body(PhysicsBody *body, double degrees);
+
+void inf_screen(PhysicsBody *body);
