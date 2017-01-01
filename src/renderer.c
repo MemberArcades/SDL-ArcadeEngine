d4be9099ec3581663ba69005e799fce9f8733be5
BanyRule <Banyrule@gmail.com>
2017-01-01 18:27:05 +0700

    Заменил структуру Point аналогичной ей Vector

--- src/renderer.c
+++ src/renderer.c
@@ -9,16 +9,16 @@
 #include "window.h"
 #include "player.h"
 
-void draw_image_coord(SDL_Surface *surface, Point dst) 
+void draw_image_coord(SDL_Surface *surface, Vector dst) 
 {
-	Point nulOffset;
+	Vector nulOffset;
 	nulOffset.x = 0;
 	nulOffset.y = 0;
 
 	draw_image_coord_offset(surface, dst, nulOffset);
 }
 
-void draw_image_coord_offset(SDL_Surface *surface, Point dst, Point offset)
+void draw_image_coord_offset(SDL_Surface *surface, Vector dst, Vector offset)
 {
 	SDL_Rect dstrect;
 
@@ -28,37 +28,46 @@ void draw_image_coord_offset(SDL_Surface *surface, Point dst, Point offset)
 	SDL_BlitSurface(surface, NULL, get_screen(), &dstrect);
 }
 
-void draw_point(SDL_Surface *surface, Point dst, Uint32 color)
+void draw_point(SDL_Surface *surface, Vector dst, Uint32 color)
 {
-	SDL_Rect fillRect = { dst.x, dst.y, 1, 1 };
-	SDL_FillRect(surface, &fillRect, color);
+	/* color format: 0xRRGGBBAA */
+	pixelColor(surface, dst.x, dst.y, color);
 }
 
-void draw_line(SDL_Surface *surface, Point begin, Point end, Uint32 color) 
+void draw_line(SDL_Surface *surface, Vector begin, Vector end, Uint32 color) 
 {
-	Point point;
-	point.x = end.x - begin.x;
-	point.y = end.y - begin.y;
-	double length = sqrt(point.x * point.x + point.y * point.y);
+	/* color format: 0xRRGGBBAA */
+	lineColor(surface, begin.x, begin.y, end.x, end.y, color);
+}
 
-	Point add;
-	add.x = point.x / length;
-	add.y = point.y / length;
+void draw_physics_body(SDL_Surface *surface, PhysicsBody body)
+{
+	int total = body.collider.points_count;
+	Vector pos = body.position;
 
-	point.x = begin.x;
-	point.y = begin.y;
+	Vector first_point = sum_vector(pos, body.collider.points[0]);
+	Vector last_point = sum_vector(pos, body.collider.points[total - 1]);
 
-	for (int i = 0; i < length; i += 1) 
+	for (size_t i = 1; i < total; ++i)
 	{
-		draw_point(surface, point, color);
-		point.x += add.x;
-		point.y += add.y;
+		draw_line
+		(
+			surface,
+			sum_vector(pos, body.collider.points[i - 1]),
+			sum_vector(pos, body.collider.points[i]),
+			0xFFFFFFFF
+		);
 	}
+
+	draw_line(surface, first_point, last_point, 0xFFFFFFFF);
 }
 
 void draw_player(SDL_Surface *surface, Player player)
 {
-	Point a = { player.body.position.x,  player.body.position.y };
-	Point b = { a.x + player.body.direction.x * 16, a.y + player.body.direction.y * 16 };
-	draw_line(surface, a, b, SDL_MapRGBA(surface->format, 0xFF, 0xFF, 0xFF, 0xFF));
+	draw_physics_body(surface, player.body);
+}
+
+void draw_enemy(SDL_Surface *surface, Enemy enemy)
+{
+	draw_physics_body(surface, enemy.body);
 }
--- src/renderer.h
+++ src/renderer.h
@@ -4,19 +4,17 @@
 #include "SDL.h"
 
 #include "player.h"
+#include "enemy.h"
+#include "vector.h"
 
-typedef struct
-{
-	double x;
-	double y;
-} Point;
+void draw_image_coord(SDL_Surface *surface, Vector dst);
 
-void draw_image_coord(SDL_Surface *surface, Point dst);
+void draw_image_coord_offset(SDL_Surface *surface, Vector dst, Vector offset);
 
-void draw_image_coord_offset(SDL_Surface *surface, Point dst, Point offset);
+void draw_point(SDL_Surface *surface, Vector dst, Uint32 color);
 
-void draw_point(SDL_Surface *surface, Point dst, Uint32 color);
-
-void draw_line(SDL_Surface *surface, Point begin, Point end, Uint32 color);
+void draw_line(SDL_Surface *surface, Vector begin, Vector end, Uint32 color);
 
 void draw_player(SDL_Surface *surface, Player player);
+
+void draw_enemy(SDL_Surface *surface, Enemy enemy);
