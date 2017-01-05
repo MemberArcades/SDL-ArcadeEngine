2c734341f481d67bb286ef2ca272aad6b1b800e8
BanyRule <Banyrule@gmail.com>
2017-01-05 17:21:54 +0700

    Добавлено: проверка столкновений с астероидами

--- src/enemy.c
+++ src/enemy.c
@@ -10,9 +10,11 @@ Vector bodies[3][12] = {
 	{ { 10, 0 },{ 27, 0 },{ 40, 10 },{ 27, 20 },{ 40, 30 },{ 33, 40 },{ 27, 36 },{ 10, 40 },{ 0, 25 },{ 0, 10 },{ 17, 10 },{ 17, 10 } },
 };
 
-void enemy_init(Enemy *enemy, Vector position, Vector direction, double velocity, int body_type)
+void enemy_init(Enemy *enemy, Vector position, Vector direction, double velocity, int body_type, int size)
 {
 	enemy->alive = true;
+	enemy->size = size;
+	enemy->body_type = body_type;
 
 	enemy->body.position = position;
 	enemy->body.direction = direction;
@@ -20,8 +22,10 @@ void enemy_init(Enemy *enemy, Vector position, Vector direction, double velocity
 
 	enemy->body.collider.points_count = 12;
 
-	for (int i = 0; i < 12; ++i)
-		enemy->body.collider.points[i] = bodies[body_type][i];
+	for (int i = 0; i < 12; ++i){
+		Vector temp = bodies[body_type][i]; 
+		enemy->body.collider.points[i] = temp;
+	}
 }
 
 void enemies_init(Enemy enemies[], int n)
@@ -36,7 +40,7 @@ void enemies_init(Enemy enemies[], int n)
 		normalise_vector(&dir);
 		int body_type = rand() % 3;
 
-		enemy_init(&enemies[i], pos, dir, 1, body_type);
+		enemy_init(&enemies[i], pos, dir, 1, body_type, 2);
 	}
 }
 
@@ -44,14 +48,67 @@ void enemy_reset_to_position(Enemy enemies[], Vector position)
 {
 	/* Найти первую свободную ячейку */
 	int i = 0;
-	for (i = 0; enemies[i].alive && i < 16; ++i); 
+	for (i = 0; (enemies[i].alive ||  enemies[i].size < 1) && i < MAX_ENEMIES; ++i);
+
+	/* Нет свободных ячеек */
+	if (enemies[i].alive || enemies[i].size < 1)
+		return;
 
 	/* Вернуть астероид в указаное место */
 	enemies[i].alive = true;
 	enemies[i].body.position = position;
+	
+
+	
 }
 
-void enemy_remove(Enemy enemies[], int number)
+void enemy_remove(Enemy *enemy)
 {
-	enemies[number].alive = false;
+	enemy->alive = false;
+
+	enemy->size -= 1;
+	
+	for (int j = 0; j < enemy->body.collider.points_count; ++j)
+		mul_vector(&enemy->body.collider.points[j], 0.75);
+}
+
+/* Тут я поленился реализовывать crossing number (count) algorithm.
+* Поэтому разбил врагов (благо, что их всего 3 типа) на 9 треугольников
+* и проверял их на принадлежность точки.
+* \triangles - индексы вершин треугольника в ps.body.collider.points[]
+*/
+bool enemy_collision_with(Enemy enemy, PhysicsBody body)
+{
+	if (!enemy.alive)
+		return false;
+
+	int triangles[3][9][3] = {
+		{ { 2, 0, 1 },{ 2, 3, 4 },{ 2, 4, 5 },{ 2, 5, 6 },{ 2, 6, 7 },{ 2, 7, 8 },{ 2, 8, 9 },{ 2, 9, 0 },{ 2, 9, 0 } },
+		{ { 1, 2, 3 },{ 1, 3, 4 },{ 1, 4, 5 },{ 1, 5, 6 },{ 1, 6, 7 },{ 1, 7, 8 },{ 1, 8, 9 },{ 1, 9, 10 },{ 1, 10, 11 } },
+		{ { 10, 0, 1 },{ 10, 1, 2 },{ 10, 2, 3 },{ 10, 3, 4 },{ 10, 4, 5 },{ 10, 5, 6 },{ 10, 6, 7 },{ 10, 7, 8 },{ 10, 8, 9 } } 
+	};
+
+	for (int i = 0; i < body.collider.points_count; ++i)
+	{
+		Vector p = sum_vector(body.collider.points[i], body.position);
+		
+		for (int j = 0; j < 9; ++j)
+		{
+			/* Странная ошибка, когда body_type не определен */
+			/*if (enemy.body_type < 0)
+				enemy.body_type *= -1;
+			enemy.body_type %= 3;*/
+
+			Vector a = sum_vector(enemy.body.collider.points[ triangles[enemy.body_type][j][2] ], enemy.body.position);
+			Vector b = sum_vector(enemy.body.collider.points[ triangles[enemy.body_type][j][1] ], enemy.body.position);
+			Vector c = sum_vector(enemy.body.collider.points[ triangles[enemy.body_type][j][0] ], enemy.body.position);
+
+			if (point_in_triangle(p, a, b, c))
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
 }
--- src/enemy.h
+++ src/enemy.h
@@ -2,18 +2,23 @@
 
 #include "physicsbody.h"
 
+#define MAX_ENEMIES 32
+
 typedef struct
 {
 	bool alive;
 	int size;
+	int body_type;
 	PhysicsBody body;
 	/* ... */
 } Enemy;
 
-void enemy_init(Enemy *enemy, Vector position, Vector direction, double velocity, int body_type);
+void enemy_init(Enemy *enemy, Vector position, Vector direction, double velocity, int body_type, int size);
 
 void enemies_init(Enemy enemies[], int n);
 
 void enemy_reset_to_position(Enemy enemies[], Vector position);
 
-void enemy_remove(Enemy enemies[], int number);
+void enemy_remove(Enemy *enemy);
+
+bool enemy_collision_with(Enemy enemy, PhysicsBody body);
