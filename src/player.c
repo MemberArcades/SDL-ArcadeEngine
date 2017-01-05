92ce21d6b978600b621470461a510e3fb46e7e02
BanyRule <Banyrule@gmail.com>
2017-01-05 17:18:13 +0700

    Изменил collider игрока

--- src/player.c
+++ src/player.c
@@ -7,14 +7,14 @@ void player_init(Player *player)
 	player->thrust = false;
 	player->angle = 0;
 
-	player->body.position = (Vector) { 15, 15 };
+	player->body.position = (Vector) { 150, 150 };
 	player->body.direction = (Vector) { 0, 1 };
 	player->body.velocity = 1;
 
 	player->body.collider.points_count = 3;
-	player->body.collider.points[0] = (Vector) { -5, 0 };
+	player->body.collider.points[0] = (Vector) { 0, 15 };
 	player->body.collider.points[1] = (Vector) { 5, 0 };
-	player->body.collider.points[2] = (Vector) { 0, 15 };
+	player->body.collider.points[2] = (Vector) { -5, 0 };
 }
 
 void move_player(Player *player, double pwr)
