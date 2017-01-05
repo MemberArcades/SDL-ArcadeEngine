aa037cfa1c9b3058abdd3d1561107e0198c12fbc
BanyRule <Banyrule@gmail.com>
2017-01-05 17:23:02 +0700

    Добавлено: проверка принадлежности точки треугольнику

--- src/vector.c
+++ src/vector.c
@@ -2,6 +2,7 @@
 
 #include <math.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 void print_vector(Vector v)
 {
@@ -17,6 +18,11 @@ void add_vector(Vector *v, Vector u)
 Vector sum_vector(Vector v, Vector u)
 {
 	return (Vector) { v.x + u.x, v.y + u.y };
+}
+
+Vector sub_vector(Vector v, Vector u)
+{
+	return (Vector) { v.x - u.x, v.y - u.y };
 }
 
 void mul_vector(Vector *v, double n)
@@ -37,6 +43,11 @@ void normalise_vector(Vector *v)
 	v->y /= magnitude;
 }
 
+bool equal_vector(Vector v, Vector u)
+{
+	return (v.x == u.x && v.y == u.y);
+}
+
 void rotate_vector(Vector *v, double degrees)
 {
 	double angle = degrees * M_PI / 180;
@@ -50,4 +61,34 @@ void rotate_vector(Vector *v, double degrees)
 
 	v->x = x * rot_matrix[0][0] + y * rot_matrix[0][1];
 	v->y = x * rot_matrix[1][0] + y * rot_matrix[1][1];
+}
+
+int classify(Vector p, Vector e0, Vector e1)
+{
+	Vector a = sub_vector(e1, e0);
+	Vector b = sub_vector(p, e0);
+
+	double sa = a.x * b.y - b.x * a.y;
+
+	if (sa > 0.0)
+		return LEFT;
+	if (sa < 0.0)
+		return RIGHT;
+	if ((a.x * b.x < 0.0) || (a.y * b.y < 0.0))
+		return BEHIND;
+	if (magnitude_vector(a) < magnitude_vector(b))
+		return BEYOND;
+	if (equal_vector(e0, p))
+		return ORIGIN;
+	if (equal_vector(e1, p))
+		return DESTINATION;
+
+	return BETWEEN;
+}
+
+bool point_in_triangle(Vector p, Vector a, Vector b, Vector c)
+{
+	return ((classify(p, a, b) != LEFT) &&
+		(classify(p, b, c) != LEFT) &&
+		(classify(p, c, a) != LEFT));
 }
--- src/vector.h
+++ src/vector.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdbool.h>
+
 #define _USE_MATH_DEFINES
 
 typedef struct 
@@ -8,11 +10,23 @@ typedef struct
 	double y;
 } Vector;
 
+enum {
+	LEFT,
+	RIGHT,
+	BEYOND,
+	BEHIND,
+	BETWEEN,
+	ORIGIN,
+	DESTINATION
+};
+
 void print_vector(Vector v);
 
 void add_vector(Vector *v, Vector u);
 
 Vector sum_vector(Vector v, Vector u);
+
+Vector sub_vector(Vector v, Vector u);
 
 void mul_vector(Vector *v, double n);
 
@@ -21,3 +35,16 @@ double  magnitude_vector(Vector v);
 void normalise_vector(Vector *v);
 
 void rotate_vector(Vector *v, double degrees);
+
+/* Возвращается значение типа перечисления, указывающее на положение точки относительно отрезка
+*
+* enum {LEFT,  RIGHT,  BEYOND,  BEHIND, BETWEEN, ORIGIN, DESTINATION};
+*       СЛЕВА, СПРАВА, ВПЕРЕДИ, ПОЗАДИ, МЕЖДУ,   НАЧАЛО, КОНЕЦ
+*
+*\param p: точка
+*\param e0, e1: начало и конец отрезка
+*/
+int classify(Vector p, Vector e0, Vector e1);
+
+/* Определяет принадлежность точки полигону */
+bool point_in_triangle(Vector p, Vector a, Vector b, Vector c);
